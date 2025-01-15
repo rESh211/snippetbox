5d5c592a9cfa135b4ad509bca6a976f37feb28de
Your Name <55reshetnikov@mail.ru>
2025-01-15 16:39:25 +0500

    1

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -14,8 +16,27 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-
-	w.Write([]byte("Hello"))
+	// Инициализируем срез содержащий пути к двум файлам.
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+	// Используем функцию template.ParseFiles() для чтения файла шаблона.
+	// Если возникла ошибка,ответ: 500 Internal Server Error (Внутренняя ошибка на сервере).
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	// Затем мы используем метод Execute() для записи содержимого шаблона в тело HTTP ответа.
+	//Последний параметр в Execute() предоставляет возможность отправки динамических данных в шаблон.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 // Обработчик для отображения содержимого заметки.
