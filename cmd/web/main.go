627c2dec75996b6ca25c4f8cd5c92e7b14d65711
Your Name <55reshetnikov@mail.ru>
2025-01-15 21:38:09 +0500

    1

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
@@ -34,22 +35,47 @@ func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
 }
 
 func main() {
-	//Создаем новый флаг командной строки, значение по умолчанию: ":400
+	//Создаем новый флаг командной строки, значение по умолчанию: ":400.
 	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
 	flag.Parse()
 
-	// Регистрируем два новых обработчика и соответствующие URL-шаблоны в
+	// Открываем файл для записи логов.
+	f, err := os.OpenFile("info.log", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// Создаем логгер для записи информационных сообщений.
+	infoLog := log.New(f, "INFO\t", log.Ldate|log.Ltime)
+	// Создаем логгер для записи сообщений об ошибках.
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Регистрируем два новых обработчика и соответствующие URL-шаблоны.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Настройка файлового сервера.
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
 	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// Создаем сервер.
+	srv := &http.Server{
+		// Адрес, на котором сервер будет слушать.
+		Addr: *addr,
+		// Лог для ошибок.
+		ErrorLog: errorLog,
+		// Мультиплексор для маршрутизации запросов.
+		Handler: mux,
+	}
+
 	// Запускаем веб-сервер и записываем лог.
-	log.Printf("Запуск сервера на %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	infoLog.Printf("Запуск сервера на %s", *addr)
+	err = srv.ListenAndServe()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 }
